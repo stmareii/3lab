d8cac1ac80d1e567526b30dcf56e39e5b5bccfb5
stmareii <masha.erendjenova@gmail.com>
2024-12-18 02:48:02 +0300

    start button

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
 from ui_mainwindow import Ui_MainWindow
+import random
+from PyQt6.QtCore import QTimer, Qt
+
 
 
 class MainWindow(QMainWindow):
@@ -11,13 +14,24 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.startButton.clicked.connect(self.hello)
+        self.ui.startButton.clicked.connect(self.startTest)
+        self.level = 1  # текущий уровень сложности
+        self.sequence = []  # Последовательность чисел вывод на экран
+        self.show_time = 2000  # Время показа чисел в миллисекундах
 
-    def hello(self):
-        text = self.ui.lineEdit.text()
-        if text == "":
-            text = "Oo"
-        self.ui.centretext.setText(f"Проверка, {text}!!!")
+    # здесь, при нажатии кнопки старт, мы получим рандом числа на экран на 2 сек
+    def startTest(self):
+        # text = self.ui.lineEdit.text() это проверка кода с ютуба хдхд
+        # if text == "":
+        #     text = "Oo"
+        # self.ui.centretext.setText(f"Проверка, {text}!!!")
+        self.sequence = [random.randint(0,9) for _ in range(self.level + 2)]
+        self.ui.centretext.setText(" ".join(map(str, self.sequence)))
+        self.ui.lineEdit.setText("")
+        self.ui.lineEdit.setEnabled(False)
+        self.ui.checkButton.setEnabled(False) #блок кнопки и ввода
+        QTimer.singleShot(...) #дописать после хайднамберс
+
 
 
 if __name__ == "__main__":
