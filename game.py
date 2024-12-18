cd2c5044d69f6c763b6c6cde32df7d652375572b
stmareii <masha.erendjenova@gmail.com>
2024-12-18 03:34:19 +0300

    very crude but working version :^

--- game.py
+++ game.py
@@ -4,6 +4,8 @@ from ui_mainwindow import Ui_MainWindow
 import random
 from PyQt6.QtCore import QTimer, Qt
 
+# короче изменить переход между проверкой и следующим раундом,а также во время некст раундов менять кнопку "старт" на "продллжить"
+# либо просто как то упростить себе жизнь ... можно добавить всплывающее окно(?) после "победы" или "проигрыша", дабы не менять главный текст?...
 
 
 class MainWindow(QMainWindow):
@@ -15,6 +17,8 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.startButton.clicked.connect(self.startTest)
+        self.ui.checkButton.clicked.connect(self.checkAnswer)
+
         self.level = 1  # текущий уровень сложности
         self.sequence = []  # Последовательность чисел вывод на экран
         self.show_time = 2000  # Время показа чисел в миллисекундах
@@ -25,13 +29,38 @@ class MainWindow(QMainWindow):
         # if text == "":
         #     text = "Oo"
         # self.ui.centretext.setText(f"Проверка, {text}!!!")
-        self.sequence = [random.randint(0,9) for _ in range(self.level + 2)]
+        self.sequence = [random.randint(0, 9) for _ in range(self.level + 2)]
         self.ui.centretext.setText(" ".join(map(str, self.sequence)))
         self.ui.lineEdit.setText("")
         self.ui.lineEdit.setEnabled(False)
-        self.ui.checkButton.setEnabled(False) #блок кнопки и ввода
-        QTimer.singleShot(...) #дописать после хайднамберс
+        self.ui.checkButton.setEnabled(False)  # блок кнопки и ввода
+        QTimer.singleShot(self.show_time, self.hideNumbers)
 
+    def hideNumbers(self):
+        self.ui.centretext.setText("Введите числа в правильном порядке:")
+        self.ui.lineEdit.setEnabled(True)
+        self.ui.checkButton.setEnabled(True)
+        self.ui.lineEdit.setFocus()
+
+    def checkAnswer(self):
+        user_in = self.ui.lineEdit.text().strip()
+        user_sequence = list(map(str, user_in.split()))
+
+        if user_sequence == list(map(str, self.sequence)):
+            self.ui.centretext.setText(
+                "Excellent!! Вы ввели правильную последовательность."
+            )
+            self.level += 1
+            self.show_time = max(1000, self.show_time - 200)
+        else:
+            self.ui.centretext.setText(
+                f"Неверно:(\nПравильная последовательность была: {' '.join(map(str, self.sequence))})"
+            )
+            self.level = 1
+            self.show_time = 2000
+        self.ui.lineEdit.setEnabled(False)
+        self.ui.checkButton.setEnabled(False)
+        self.ui.centretext.setText("Нажмите 'старт', чтобы продолжить")
 
 
 if __name__ == "__main__":
